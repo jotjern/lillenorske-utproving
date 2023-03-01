1c02fabe7cf24480178ab47d487ec486c3cb7f01
jogt <jotjernshaugen@gmail.com>
2023-03-01 13:54:33 +0100

    Changed thank you message

--- b_frontend/src/components/templates/ThanksPage/index.tsx
+++ b_frontend/src/components/templates/ThanksPage/index.tsx
@@ -3,7 +3,7 @@ import SurveyFrame from "../../organisms/SurveyFrame";
 export default () => {
     return <SurveyFrame>
         <span style={{fontSize: "1.5em"}}>
-            <h1>Takk for at du deltok!</h1>
+            <h1>Takk for at du var med!</h1>
             <p>Du kan nå lukke siden</p>
         </span>
     </SurveyFrame>
